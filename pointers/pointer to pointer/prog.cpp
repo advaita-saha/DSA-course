b9da926a9064e4bfa63c67cc73ff90aef7467398
Advaita Saha <adv11sphs@gmail.com>
2021-10-19 13:41:12 +0530

    pointer to pointer start and gitignore

---
+++ pointers/pointer to pointer/prog.cpp
@@ -0,0 +1,13 @@
+/*
+Swapping 2 variables using the address
+Address of ‘a’ and ‘b’ gets interchanged, thus their values get swapped.
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    
+
+    return 0; 
+}
