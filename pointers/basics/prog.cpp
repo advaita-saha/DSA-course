639b280cd045da5dd04717e970eb26a6d254e197
Advaita Saha <adv11sphs@gmail.com>
2021-10-19 13:17:48 +0530

    pointers basic

---
+++ pointers/basics/prog.cpp
@@ -0,0 +1,12 @@
+/*
+Pointers are variables that store the address of other variables.
+Syntax : <data_type> *name_of_pointer
+
+Getting started with the Pointers
+Every variable is stored in the memory and each memory location has its own
+memory address. It enables us to pass variables by reference.
+
+‘&’ Operator: It gives the address of the variable.
+‘*’ Operator: It gives the value stored at the address, i.e dereferences the value
+stored at the address
+*/
